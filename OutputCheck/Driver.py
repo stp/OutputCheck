f83e467a934e114b1dc9b17bea72390f9406c4d3
Dan Liew <daniel.liew@imperial.ac.uk>
2014-03-02 13:48:40 +0000

    Fixed code that was not python 3 compatible.

--- OutputCheck/Driver.py
+++ OutputCheck/Driver.py
@@ -18,7 +18,7 @@ _logger = logging.getLogger(__name__)
 
 def enum(*sequential, **named):
     enums = dict(zip(sequential, range(len(sequential))), **named)
-    reverse = dict((value, key) for key, value in enums.iteritems())
+    reverse = dict((value, key) for key, value in enums.items())
     enums['reverse_mapping'] = reverse
     return type('Enum', (), enums)
 
