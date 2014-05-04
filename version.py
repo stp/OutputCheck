1a8abb727167ff7361743ea9ca5547ef0045ec15
Dan Liew <daniel.liew@imperial.ac.uk>
2014-05-04 22:43:34 +0100

    Fix packaging problem where git tag was parsed but we got bytes back so setup.py script would create wrong name for PyPi package.

--- version.py
+++ version.py
@@ -97,7 +97,7 @@ def get_git_version(abbrev=4):
 
     # Finally, return the current version.
 
-    return version
+    return version.decode() # Get str from bytes for Python 3.x
 
 
 if __name__ == "__main__":
